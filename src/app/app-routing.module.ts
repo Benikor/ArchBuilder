67cc9d5c36b38cac1b6dd4a24853bd23d8d6baae
benikor <kormos.benedek@gmail.com>
2022-05-26 23:42:16 +0200

    Implement routing and Top Bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SkillBuilderComponent } from './skill-builder/skill-builder.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'sorcerer', component: SkillBuilderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
