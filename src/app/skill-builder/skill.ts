da833dd3079f7400efa6781c35264b7931e29a1e
benikor <kormos.benedek@gmail.com>
2023-10-20 12:43:34 +0200

    Finalize sorcerer skills data. Update skill component location.

--- src/app/skill-builder/skill.ts
+++ src/app/skill-builder/skill.ts
@@ -1,4 +1,12 @@
 export interface Skill {
   name: string;
   imgPath: string;
+  level: number;
+  description: string;
+  skillPoints: [number];
+  heroicPoints: [number];
+  requiredLevels: [number];
+  requiredSkills: {};
+  requiredHeroicPoints: number;
+  stats: {};
 }
