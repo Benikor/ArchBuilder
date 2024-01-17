491224947c6aa796da5e3d7b71a6beed9ef41792
benikor <kormos.benedek@gmail.com>
2024-01-17 04:13:07 +0100

    Remove unused component from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SkillBuilderComponent } from './skill-builder/skill-builder.component';
-import { BasicSkillsComponent } from './skill-builder/basic-skills/basic-skills.component';
-import { ComboSkillsComponent } from './skill-builder/combo-skills/combo-skills.component';
-import { HeroicSkillsComponent } from './skill-builder/heroic-skills/heroic-skills.component';
 import { SkillComponent } from './skill-builder/skill/skill.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,9 +16,6 @@ import { SkillDescriptionComponent } from './skill-builder/skill/skill-descripti
   declarations: [
     AppComponent,
     SkillBuilderComponent,
-    BasicSkillsComponent,
-    ComboSkillsComponent,
-    HeroicSkillsComponent,
     SkillComponent,
     MainPageComponent,
     HeaderComponent,
