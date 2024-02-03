2a9266b58a9f33fca18bbccb9b87e28e97dc21d9
Benikor <kormos.benedek@gmail.com>
2024-02-03 10:51:14 +0100

    Mark the active class in the class-menu

--- src/app/skill-builder/class-menu/class-menu.component.ts
+++ src/app/skill-builder/class-menu/class-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SkillBuilderService } from '../skill-builder.service';
 
 @Component({
@@ -7,6 +7,7 @@ import { SkillBuilderService } from '../skill-builder.service';
   styleUrls: ['./class-menu.component.css'],
 })
 export class ClassMenuComponent implements OnInit {
+  @Input() currentClass: string = '';
   classes: string[] = [];
 
   constructor(private skillBuilderService: SkillBuilderService) {}
